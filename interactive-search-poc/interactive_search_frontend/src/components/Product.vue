a41d25550d11f7f506d85d471e113cab0d05d94f
Aysenur Bilgin <aysenurbilgin@gmail.com>
2021-03-04 13:41:24 +0100

    Adding the web app

---
+++ interactive-search-poc/interactive_search_frontend/src/components/Product.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-card
+      class="mx-auto"
+  >
+    <v-img
+        :src="details['_source']['imUrl']"
+        height="200px"
+    ></v-img>
+
+    <v-card-subtitle class="body-2 pl-2">
+      <div class="headerClass">
+        {{ details["_source"]["title"] }}
+      </div>
+    </v-card-subtitle>
+
+    <v-chip
+        class="ma-1"
+        color="#1F22A9"
+        label
+        text-color="white"
+    >
+      <v-icon left>
+        mdi-label
+      </v-icon>
+      EUR {{ details["_source"]["price"] }}
+    </v-chip>
+
+    <v-card-actions>
+      <v-btn
+          color="orange lighten-2"
+          text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+          icon
+          @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text> {{ details["_source"]["description"] }} </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Product",
+  props: ['details'],
+  data: () => ({
+    show: false,
+  }),
+}
+</script>
+
+<style scoped>
+.headerClass{
+  white-space: nowrap ;
+  word-break: normal;
+  overflow: hidden ;
+  text-overflow: ellipsis;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
---
+++ interactive-search-poc/interactive_search_frontend/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    proxy: 'http://localhost:5050'
+  },
+  transpileDependencies: [
+    'vuetify'
+  ]
+}
